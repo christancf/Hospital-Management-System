657a098f8635580e07c04d07ca596c3863e7ab2f
Shavidini Ekanayake <shavidilunika10s@gmail.com>
2022-03-23 23:15:41 +0530

    Fixed refesh botton in add blood bag

--- Frontend/src/views/bloodbank-views/add-blood-bag/index.js
+++ Frontend/src/views/bloodbank-views/add-blood-bag/index.js
@@ -1,60 +1,180 @@
-import React, { useState } from 'react';
-import {
-  Form,
-  Input,
-  Button,
-  Select,
-  DatePicker, 
-} from 'antd';
+import React from 'react';
+import { Form, Input, Button, Select,DatePicker } from 'antd';
 
 const { Option } = Select;
 
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
 const tailLayout = {
   wrapperCol: { offset: 8, span: 16 },
 };
 
-const AddBloodBag = () => {
+class AddBloodBag extends React.Component {
+  formRef = React.createRef();
 
-  return (
-    <div>
-        <Form.Item label="Donor's Name" >
+  onGenderChange = value => {
+    this.formRef.current.setFieldsValue({
+      note: `Hi, ${value === 'male' ? 'man' : 'lady'}!`,
+    });
+  };
+
+  onFinish = values => {
+    // var gen = values.gender;
+    console.log(values);
+  };
+
+  onReset = () => {
+    this.formRef.current.resetFields();
+  };
+
+  onFill = () => {
+    this.formRef.current.setFieldsValue({
+      note: 'Hello world!',
+      gender: 'male',
+    });
+  };
+
+  render() {
+    return (
+      <Form {...layout} ref={this.formRef} name="control-ref" onFinish={this.onFinish}>
+        <Form.Item name="donorsName" label="Donor's Name" >
           <Input />
         </Form.Item>
         <Form.Item label="Donor's NIC" name="DonorNIC" rules={[{ required: true, message: 'Please input Donor\'s NIC!' }]}>
-          <Input />
+        <Input />
         </Form.Item>
         <Form.Item label="Donation Number " name="DonationNum" rules={[{ required: true, message: 'Please input Donation Number!' }]}>
-          <Input />
+        <Input />
         </Form.Item>
-        <Form.Item label="Donated Date & Time">
-          <DatePicker />
+        <Form.Item label="Donated Date & Time" name="dateTime">
+        <DatePicker />
         </Form.Item>
-        <Form.Item label="Place" >
-          <Input />
-        </Form.Item>
-        <Form.Item label="Blood Group">
-          <Select>
-            <Select.Option value="A+">A positive(A+)</Select.Option>
-            <Select.Option value="A-">A negative(A-)</Select.Option>
-            <Select.Option value="B+">B positive(B+)</Select.Option>
-            <Select.Option value="B-">B negative(B-)</Select.Option>
-            <Select.Option value="O+">O positive(O+)</Select.Option>
-            <Select.Option value="O-">O negative(O-)</Select.Option>
-            <Select.Option value="AB+">AB positive(AB+)</Select.Option>
-            <Select.Option value="AB-">AB negative(AB-)</Select.Option>
-         </Select>
+        <Form.Item label="Place" name="Place">
+        <Input />
         </Form.Item>
 
+        <Form.Item name="bloodGroup" label="Blood Group" rules={[{ required: true }]}>
+          <Select
+            placeholder="Select the blood group"
+            // onChange={this.onGenderChange}
+            allowClear
+            
+          >
+            <Option value="A+">A positive(A+)</Option>
+            <Option value="A-">A negative(A-)</Option>
+            <Option value="B+">B positive(B+)</Option>
+            <Option value="A+">B negative(B-)</Option>
+            <Option value="A-">O positive(O+)</Option>
+            <Option value="B+">O negative(O-)</Option>
+            <Option value="A-">AB positive(AB+)</Option>
+            <Option value="B+">AB negative(AB-)</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          noStyle
+          shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
+        >
+          {({ getFieldValue }) => {
+            return getFieldValue('gender') === 'other' ? (
+              <Form.Item
+                name="customizeGender"
+                label="Customize Gender"
+                rules={[{ required: true }]}
+              >
+                <Input />
+              </Form.Item>
+            ) : null;
+          }}
+        </Form.Item>
         <Form.Item {...tailLayout}>
-          <Button className="mr-2" htmlType="button" >
-            Reset
-          </Button>
           <Button className="mr-2" type="primary" htmlType="submit">
             Submit
           </Button>
-      </Form.Item>
-    </div>
-  );
-};
+          <Button className="mr-2" htmlType="button" onClick={this.onReset}>
+            Reset
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
 
-export default AddBloodBag
+ export default AddBloodBag;
+
+// import React, { useState } from 'react';
+// import {
+//   Form,
+//   Input,
+//   Button,
+//   Select,
+//   DatePicker, 
+// } from 'antd';
+
+// const { Option } = Select;
+
+// const tailLayout = {
+//   wrapperCol: { offset: 8, span: 16 },
+// };
+
+// onFinish = values => {
+//   console.log(values);
+// };
+
+// onReset = () => {
+//   this.formRef.current.resetFields();
+// };
+
+// onFill = () => {
+//   this.formRef.current.setFieldsValue({
+//     note: 'Hello world!',
+//     gender: 'male',
+//   });
+// };
+
+// const AddBloodBag = () => {
+
+//   return (
+//     <div>
+//         <Form.Item label="Donor's Name" >
+//           <Input />
+//         </Form.Item>
+//         <Form.Item label="Donor's NIC" name="DonorNIC" rules={[{ required: true, message: 'Please input Donor\'s NIC!' }]}>
+//           <Input />
+//         </Form.Item>
+//         <Form.Item label="Donation Number " name="DonationNum" rules={[{ required: true, message: 'Please input Donation Number!' }]}>
+//           <Input />
+//         </Form.Item>
+//         <Form.Item label="Donated Date & Time">
+//           <DatePicker />
+//         </Form.Item>
+//         <Form.Item label="Place" >
+//           <Input />
+//         </Form.Item>
+//         <Form.Item label="Blood Group">
+//           <Select>
+//             <Select.Option value="A+">A positive(A+)</Select.Option>
+//             <Select.Option value="A-">A negative(A-)</Select.Option>
+//             <Select.Option value="B+">B positive(B+)</Select.Option>
+//             <Select.Option value="B-">B negative(B-)</Select.Option>
+//             <Select.Option value="O+">O positive(O+)</Select.Option>
+//             <Select.Option value="O-">O negative(O-)</Select.Option>
+//             <Select.Option value="AB+">AB positive(AB+)</Select.Option>
+//             <Select.Option value="AB-">AB negative(AB-)</Select.Option>
+//          </Select>
+//         </Form.Item>
+
+//         <Form.Item {...tailLayout}>
+//           <Button className="mr-2" type="primary" htmlType="submit">
+//             Submit
+//           </Button>
+//           <Button className="mr-2" htmlType="button" onClick={this.onReset}>
+//             Reset
+//           </Button>
+//         </Form.Item>
+//     </div>
+//   );
+// };
+
+// export default AddBloodBag
