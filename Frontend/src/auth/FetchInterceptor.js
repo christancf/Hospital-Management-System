f947962ab9f184fe0a3b9da3fa39169e93df7726
Susith Rupasinghe <susith03program@gmail.com>
2022-03-24 12:12:11 +0530

    Backend CORS added

--- Frontend/src/auth/FetchInterceptor.js
+++ Frontend/src/auth/FetchInterceptor.js
@@ -9,6 +9,22 @@ const service = axios.create({
   timeout: 60000
 })
 
+service.interceptors.request.use(config => {
+	
+    config.headers['Access-Control-Allow-Origin'] = 'http://localhost:8080';
+	config.headers['Access-Control-Allow-Credentials'] = 'true';
+  
+
+
+  return config
+}, error => {
+	// Do something with request error here
+	notification.error({
+		message: 'Error'
+	})
+  Promise.reject(error)
+})
+
 // // Config
 // const ENTRY_ROUTE = '/auth/login'
 // const TOKEN_PAYLOAD_KEY = 'authorization'
@@ -36,39 +52,12 @@ const service = axios.create({
 //   Promise.reject(error)
 // })
 
-// // API respone interceptor
-// service.interceptors.response.use( (response) => {
-// 	return response.data
-// }, (error) => {
+// API respone interceptor
+service.interceptors.response.use( (response) => {
+	return response.data
+}, (error) => {
 
-// 	let notificationParam = {
-// 		message: ''
-// 	}
-	
-// 	// Remove token and redirect 
-// 	if (error.response.status === 400 || error.response.status === 403) {
-// 		notificationParam.message = 'Authentication Fail'
-// 		notificationParam.description = 'Please login again'
-// 		localStorage.removeItem(AUTH_TOKEN)
-// 		history.push(ENTRY_ROUTE)
-// 		window.location.reload();
-// 	}
-
-// 	if (error.response.status === 404) {
-// 		notificationParam.message = 'Not Found'
-// 	}
-
-// 	if (error.response.status === 500) {
-// 		notificationParam.message = 'Internal Server Error'
-// 	}
-	
-// 	if (error.response.status === 508) {
-// 		notificationParam.message = 'Time Out'
-// 	}
-
-// 	notification.error(notificationParam)
-
-// 	return Promise.reject(error);
-// });
+	return Promise.reject(error);
+});
 
 export default service
