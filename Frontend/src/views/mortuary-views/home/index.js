e3d9348849afc7a6f56d68fac3fbb61141449126
Sayumi Munasinghe <it20610920@my.sliit.lk>
2022-04-18 21:28:39 +0530

    Homepage components added

--- Frontend/src/views/mortuary-views/home/index.js
+++ Frontend/src/views/mortuary-views/home/index.js
@@ -1,9 +1,13 @@
-
-import { Card, Popover } from 'antd';
-
+import { Card, Popover, Button } from 'antd';
+import mortuaryService from 'services/MortuaryService';
 
 const text = <span>Title</span>;
 const content = (props) => {
+
+  function nextPage(props) {
+    localStorage.setItem("cabinetNumber", props.letter+props.number)
+    // window.location.href='/mortuary/add'
+  }
 
   if (props.isOccupied == true) {
 
@@ -11,17 +15,18 @@ const content = (props) => {
       <div>
         <p>OCCUPIED</p>
         <p>{props.letter}{props.number}</p>
-        <button>More Info</button>
+        <Button type='primary'>More Info</Button>
+        
       </div>
     )
 
   }
   else {
-
+     
     return (
       <div>
         <p>{props.letter}{props.number}</p>
-        <button>Add Corpse</button>
+        <Button type='primary' onClick={nextPage(props)}>Add Corpse</Button>
       </div>
     )
 
@@ -32,47 +37,41 @@ const content = (props) => {
 
 const Home = () => {
 
-  const letters = ['A', 'B','C','D','E','F'];
-  const numbers = [1,2,3,4,5,6,7,8,9];
-  const response = [
-    {
-      number: 'A1',
-      NIC : '200013400692',
-      Name : 'Susith Rupasinghe'
-    },
-    {
-      number: 'A2',
-      NIC : '8873783838738V',
-      Name : 'Gotabaya Rajapakshe'
-    }
-  ]
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F'];
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   
   const cabinetArray = []
-  for(var i=0; i<letters.length; i++){
+  
+  mortuaryService.getOccupiedData().then(value => {
+    var response = value.payload
+    for (var i = 0; i < letters.length; i++) {
 
-    for(var j=0; j<numbers.length; j++){
-
-      const spaceNumber = letters[i] + numbers[j];
-
-      const isFound = response.find( (sayumi) => {  return sayumi.number == spaceNumber  } );
-
-      if( isFound == undefined){
-
-        cabinetArray.push(
-          <Cabinet letter={letters[i]} number={numbers[j]} isOccupied={false}></Cabinet>
-        );
+      for (var j = 0; j < numbers.length; j++) {
+  
+        const spaceNumber = letters[i] + numbers[j];
+  
+        const isFound = response.find((corpse) => { return corpse.cabinet_number == spaceNumber });
+  
+        if (isFound == undefined) {
+  
+          cabinetArray.push(
+            <Cabinet letter={letters[i]} number={numbers[j]} isOccupied={false}></Cabinet>
+          );
+        }
+        else {
+  
+          cabinetArray.push(
+            <Cabinet letter={letters[i]} number={numbers[j]} isOccupied={true}></Cabinet>
+          );
+  
+        }
+  
       }
-      else{
-
-        cabinetArray.push(
-          <Cabinet letter={letters[i]} number={numbers[j]} isOccupied={true}></Cabinet>
-        );
-
-      }
-
     }
+    // console.log(response)
   }
-
+  )
+console.log(cabinetArray)
   return (
     <>
       <center><h1>Mortuary Space Allocation</h1></center>
@@ -83,16 +82,25 @@ const Home = () => {
 
 
 const Cabinet = (props) => {
-
-  return (
-    <>
-      <Popover placement="bottomRight" title={text} content={content({ letter: props.letter, number: props.number , isOccupied: props.isOccupied})} arrowPointAtCenter>
+  const isOccupied = props.isOccupied
+  if (isOccupied) {
+    return (
+      <Popover placement="bottomRight" title={text} content={content({ letter: props.letter, number: props.number, isOccupied: props.isOccupied })} arrowPointAtCenter>
         <Card style={{ width: 100, borderColor: '#ff6b72', borderWidth: '3px' }} hoverable>
           <h1 className='text-center'>{props.letter}{props.number}</h1>
         </Card>
       </Popover>
-    </>
-  )
+    )
+  } else {
+    return (
+      <Popover placement="bottomRight" title={text} content={content({ letter: props.letter, number: props.number, isOccupied: props.isOccupied })} arrowPointAtCenter>
+        <Card style={{ width: 100, borderColor: 'green', borderWidth: '3px' }} hoverable>
+          <h1 className='text-center'>{props.letter}{props.number}</h1>
+        </Card>
+      </Popover>
+    )
+  }
+
 
 }
 export default Home;
