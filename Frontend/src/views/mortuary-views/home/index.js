8a7bc73f806e2a36f247add71fb5b5c8db8991ea
Sayumi Munasinghe <it20610920@my.sliit.lk>
2022-04-19 00:18:01 +0530

    Homepage view done

--- Frontend/src/views/mortuary-views/home/index.js
+++ Frontend/src/views/mortuary-views/home/index.js
@@ -1,11 +1,12 @@
-import { Card, Popover, Button } from 'antd';
+import { Card, Popover, Button, Row, Col } from 'antd';
+import { useState, useEffect } from 'react';
 import mortuaryService from 'services/MortuaryService';
 
 const text = <span>Title</span>;
 const content = (props) => {
 
   function nextPage(props) {
-    localStorage.setItem("cabinetNumber", props.letter+props.number)
+    localStorage.setItem("cabinetNumber", props.letter + props.number)
     // window.location.href='/mortuary/add'
   }
 
@@ -16,13 +17,13 @@ const content = (props) => {
         <p>OCCUPIED</p>
         <p>{props.letter}{props.number}</p>
         <Button type='primary'>More Info</Button>
-        
+
       </div>
     )
 
   }
   else {
-     
+
     return (
       <div>
         <p>{props.letter}{props.number}</p>
@@ -35,49 +36,107 @@ const content = (props) => {
 
 };
 
+const GridList = (props)=> {
+
+  const RowSet = [];
+  for (let i = 0; i < props.data.length; i += 8) {
+    const rowObj = props.data.slice(i, i + 8);
+    
+    const rowList = [];
+    for(let j = 0; j < rowObj.length; j ++){
+
+      rowList.push(
+        <Col span={3}> {rowObj[j]} </Col>
+      );
+    }
+
+     RowSet.push(<Row gutter={[48, 16]}>{rowList}</Row>);
+
+}
+
+  return(
+    <>
+    {RowSet}
+    </>
+  )
+
+}
 const Home = () => {
 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState();
+
   const letters = ['A', 'B', 'C', 'D', 'E', 'F'];
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  
-  const cabinetArray = []
-  
-  mortuaryService.getOccupiedData().then(value => {
-    var response = value.payload
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+
+  useEffect(() => {
+    mortuaryService.getOccupiedData().then((res) => {
+
+      setData(res.payload);
+      setLoading(false);
+
+    }).catch((err) => {
+      setLoading(false);
+      setError(true);
+      setData();
+    });
+  }, []);
+
+
+  if (loading) {
+    return (
+      <>
+        <center><h1>Mortuary Space Allocation</h1></center>
+        <p>Data Loading</p>
+      </>
+    )
+  }
+  else if (error) {
+    return (
+      <>
+        <center><h1>Mortuary Space Allocation</h1></center>
+        <p>Error</p>
+
+      </>
+    )
+  }
+  else {
+    const cabinetArray = [];
     for (var i = 0; i < letters.length; i++) {
 
       for (var j = 0; j < numbers.length; j++) {
-  
+
         const spaceNumber = letters[i] + numbers[j];
-  
-        const isFound = response.find((corpse) => { return corpse.cabinet_number == spaceNumber });
-  
+
+        const isFound = data.find((corpse) => { return corpse.cabinet_number == spaceNumber });
+
         if (isFound == undefined) {
-  
+
           cabinetArray.push(
             <Cabinet letter={letters[i]} number={numbers[j]} isOccupied={false}></Cabinet>
           );
         }
         else {
-  
+
           cabinetArray.push(
             <Cabinet letter={letters[i]} number={numbers[j]} isOccupied={true}></Cabinet>
           );
-  
+
         }
-  
+
       }
     }
-    // console.log(response)
+
+    return (
+      <>
+        <center><h1>Mortuary Space Allocation</h1></center>
+        <GridList data= {cabinetArray}></GridList>
+      </>
+    )
   }
-  )
-console.log(cabinetArray)
-  return (
-    <>
-      <center><h1>Mortuary Space Allocation</h1></center>
-      {cabinetArray}
-    </>
-  )
+
 }
 
 
