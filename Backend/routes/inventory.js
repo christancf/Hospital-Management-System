314c4629853b668e738b3547ab93684d8774ac7a
SanukaJay <sanukaakmaljay@gmail.com>
2022-04-19 13:22:43 +0530

    retrieve data backend

--- Backend/routes/inventory.js
+++ Backend/routes/inventory.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const auth = require("../middleware/auth");
 
 
-
+//create item in the item list
 router.post('/item', function (req, res, next) {
 
   const item = new itemModel({
@@ -32,6 +32,29 @@ router.post('/item', function (req, res, next) {
   }
 
 });
+
+
+// REtrive data from itemlist
+router.get('/itemlist', async function (req, res, next) {
+
+  try {
+    let itemDetails = await itemModel.find({}, { id: 1, item_name: 1, description: 1, manufacturer: 1, date_time_of_death: 1, date_of_release: 1, cabinet_number: 1, status: 1, _id: 0})
+    // corpseDetails = JSON.stringify(corpseDetails)  //convert to json string
+    // corpseDetails = JSON.parse(corpseDetails)  //convert to json
+    console.log(corpseDetails)
+    res.status(200).json({
+      success: true,
+      message: "Successful Retrieval",
+      payload: corpseDetails
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message
+    })
+  }
+
+})
 
 
 
