efe01a10c176ad05e2ed2cc64bdfbf3346bbb1b9
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2022-04-20 11:30:10 +0530

    modified add staff member frontend

--- Frontend/src/views/staff-views/add-staff-member/index.js
+++ Frontend/src/views/staff-views/add-staff-member/index.js
@@ -1,32 +1,35 @@
 import React from 'react'
-import { Form, Input, Button, Cascader, DatePicker, Typography } from 'antd';
+import { Form, Input, Button, Cascader, DatePicker, message, Card } from 'antd';
 import staffService from 'services/StaffService';
 
-const { Title } = Typography
+const add = 'add'
 
 const AddStaffMember = () => {
-	return (
-		<div>
-      <Title>Add New Staff Member</Title>
+  return (
+    <div>
 			<Demo />
 		</div>
 	)
 }
 
 const layout = {
-  labelCol: { span: 4 },
+  labelCol: { span: 8 },
   wrapperCol: { span: 8 },
 };
 const tailLayout = {
-  wrapperCol: { offset: 5, span: 16 },
+  wrapperCol: { offset: 8, span: 16 },
 };
 
 const Demo = () => {
+  const [form] = Form.useForm();
   const onFinish = values => {
     values.dateOfBirth = values.dateOfBirth['_d'].getTime()
 
-    staffService.addStaffMember(values);
-    console.log('Success:', values);
+    staffService.addStaffMember(values)
+      .then(() => message.success({content: 'Successfully added new staff member', add, duration: 2}))
+      .catch(() => message.error({content: 'Please try again!', add, duration: 2}))
+
+      form.resetFields();
   };
 
   const onFinishFailed = errorInfo => {
@@ -34,115 +37,126 @@ const Demo = () => {
   };
 
   return (
-    <Form
+    <Card style={{backgroundColor: '#efefef'}}>
+      <h1 className='text-left' style={{ marginLeft: 420, marginBottom: 20 }}>Add New Staff Member</h1>
+
+      <Form
       {...layout}
       name="basic"
       initialValues={{ remember: true }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
-    >
-      <Form.Item
-        label="Staff ID"
-        name="staffID"
-        rules={[{ required: true, message: 'Please input the staff ID!' }]}
+      form={form}
       >
+        <Form.Item
+          label="Staff ID"
+          name="staffID"
+          rules={[{ required: true, message: 'Please input the staff ID!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Name"
+          name="staffName"
+          rules={[{ required: true, message: 'Please input the name!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+      <Form.Item
+          label="NIC"
+          name="NIC"
+          rules={[{ required: true, message: 'Please input the NIC!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+      <Form.Item
+          label="E-mail"
+          name="email"
+          rules={[{ pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$", message: 'Please enter a valid email!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+      <Form.Item
+          label="Designation"
+          name="designation"
+          rules={[{ required: true, message: 'Please select the designation!' }]}
+        >
+        <Cascader options={designationOptions} />
+        </Form.Item>
+
+        <Form.Item
+          label="Qualification"
+          name="qualification"
+          rules={[{ required: true, message: 'Please input the qualification!' }]}
+        >
         <Input />
-      </Form.Item>
+        </Form.Item>
 
-      <Form.Item
-        label="Name"
-        name="staffName"
-        rules={[{ required: true, message: 'Please input the name!' }]}
-      >
+        <Form.Item 
+          label="Date Of Birth"
+          name="dateOfBirth"
+          rules={[{ required: true, message: 'Please input the date of birth!'}]}
+        >
+            <DatePicker />
+        </Form.Item>
+
+        <Form.Item
+          label="Gender"
+          name="gender"
+          rules={[{ required: true, message: 'Please select the gender!' }]}
+        >
+        <Cascader options={genderOptions} />
+        </Form.Item>
+
+        <Form.Item
+          label="Address"
+          name="address"
+          rules={[{ required: true, message: 'Please input the address!' }]}
+        >
         <Input />
-      </Form.Item>
+        </Form.Item>
 
-	  <Form.Item
-        label="NIC"
-        name="NIC"
-        rules={[{ required: true, message: 'Please input the NIC!' }]}
-      >
+        <Form.Item
+          label="Base Salary"
+          name="basicSalary"
+          rules={[{ required: true, message: 'Please input the base salary!' }]}
+        >
         <Input />
-      </Form.Item>
+        </Form.Item>
 
-	  <Form.Item
-        label="E-mail"
-        name="email"
-        rules={[{ pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$", message: 'Please enter a valid email!' }]}
-      >
+        <Form.Item
+          label="Mobile"
+          name="mobile"
+          rules={[{ required: true, message: 'Please input the mobile number!' }]}
+        >
         <Input />
-      </Form.Item>
+        </Form.Item>
 
-	  <Form.Item
-        label="Designation"
-        name="designation"
-        rules={[{ required: true, message: 'Please select the designation!' }]}
-      >
-		  <Cascader options={designationOptions} />
-      </Form.Item>
+        <Form.Item
+          label="Home"
+          name="home"
+          rules={[{ required: true, message: 'Please input the home number!' }]}
+        >
+        <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Qualification"
-        name="qualification"
-        rules={[{ required: true, message: 'Please input the qualification!' }]}
-      >
-		  <Input />
-      </Form.Item>
+        <Form.Item {...tailLayout}>
 
-      <Form.Item 
-        label="Date Of Birth"
-        name="dateOfBirth"
-        rules={[{ required: true, message: 'Please input the date of birth!'}]}
-      >
-          <DatePicker />
-      </Form.Item>
+          <Button htmlType="reset" style={{marginRight: 215}}>
+            Discard
+          </Button>
 
-      <Form.Item
-        label="Gender"
-        name="gender"
-        rules={[{ required: true, message: 'Please select the gender!' }]}
-      >
-		  <Cascader options={genderOptions} />
-      </Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
 
-      <Form.Item
-        label="Address"
-        name="address"
-        rules={[{ required: true, message: 'Please input the address!' }]}
-      >
-		  <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Base Salary"
-        name="basicSalary"
-        rules={[{ required: true, message: 'Please input the base salary!' }]}
-      >
-		  <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Mobile"
-        name="mobile"
-        rules={[{ required: true, message: 'Please input the mobile number!' }]}
-      >
-		  <Input />
-      </Form.Item>
-
-      <Form.Item
-        label="Home"
-        name="home"
-        rules={[{ required: true, message: 'Please input the home number!' }]}
-      >
-		  <Input />
-      </Form.Item>
-
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        </Form.Item>
+      </Form>
+    </Card>
   );
 };
 
