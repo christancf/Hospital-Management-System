92590564dd8e36ba5deb03ff1a8d3a5eeaa1ce42
Shavidini Ekanayake <shavidilunika10s@gmail.com>
2022-05-15 19:31:14 +0530

    start to implemet home page

--- Frontend/src/views/bloodbank-views/blood-bags-informations/index.js
+++ Frontend/src/views/bloodbank-views/blood-bags-informations/index.js
@@ -1,16 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect,Modal,Component } from 'react';
 import { Table, Typography, Spin, Button, Divider, Tag } from 'antd';
 import bloodBankService from 'services/BloodBankService'
 import moment from 'moment';
 
 const { Title } = Typography
 
+
 const BloodBags = () => {
+	const state = { visible: false };
 
 	function toTimestamp(strDate) {
 		var datum = Date.parse(strDate);
 		return datum / 1000;
 	}
+
+	const showModal = () => {
+		this.setState({
+		  visible: true,
+		});
+	  };
 
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
@@ -219,9 +227,18 @@ const BloodBags = () => {
 				title: 'Action',
 				key: 'action',
 				render: (text, record) => (
+
+					// <div class="ant-dropdown-trigger ellipsis-dropdown ant-dropdown-open">
+					// 	<span role="img" aria-label="ellipsis" class="anticon anticon-ellipsis">
+					// 		<svg viewBox="64 64 896 896" focusable="false" data-icon="ellipsis" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+					// 			<path d="M176 511a56 56 0 10112 0 56 56 0 10-112 0zm280 0a56 56 0 10112 0 56 56 0 10-112 0zm280 0a56 56 0 10112 0 56 56 0 10-112 0z"></path>
+					// 		</svg>
+					// 	</span>
+					// </div>
 					<span>
 						<a href={`../bloodbank/add-transfusion?bagId=${record.bagId}`}>Transfusion </a>
 						<a href={`../bloodbank/update-details?bagId=${record.bagId}`}>Edit</a>
+						<a onClick={showModal}>View More</a>
 						<Divider type="vertical" />
 						
 					</span>
@@ -396,6 +413,7 @@ const BloodBags = () => {
 
 		return (
 			<div>
+				
 				<Title>Blood Bags Details</Title>
 				<Button type="primary" href='/bloodbank/add-details'>Add Blood Bag</Button>
 				<br></br>
@@ -403,6 +421,10 @@ const BloodBags = () => {
 
 				<Title>Blood Transfusion Details</Title>
 				<Table columns={column} dataSource={bloodTransfusion} />
+				<Modal
+          title="Basic Modal"
+          visible={state.visible}
+        ></Modal>
 			</div>
 		)
 
