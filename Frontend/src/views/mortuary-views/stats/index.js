aff47addf36f13d843e456fac530e74e8747f8d2
Sayumi Munasinghe <it20610920@my.sliit.lk>
2022-05-17 14:00:03 +0530

    One graph in stats page done

--- Frontend/src/views/mortuary-views/stats/index.js
+++ Frontend/src/views/mortuary-views/stats/index.js
@@ -1,11 +1,83 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Chart from "react-apexcharts";
+import { COLOR_2 } from "constants/ChartConstant";
+import mortuaryService from "services/MortuaryService";
 
 const Home = () => {
-	return (
-		<div>
-			stats!
-		</div>
-	)
-}
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [key, setKey] = useState(null);
+  const [value, setValue] = useState(null);
 
-export default Home
+  useEffect(() => {
+    mortuaryService
+      .stat()
+      .then((res) => {
+        const myData = res.payload;
+        var key = [];
+        var value = [];
+
+        for (let i = 0; i < myData.length; i++) {
+          key[i] = myData[i]._id;
+          value[i] = myData[i].count;
+        }
+        setKey(key);
+        setValue(value);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+        setKey();
+        setValue();
+      });
+  }, []);
+  const series = [
+    {
+      name: "Desktops",
+      data: value,
+    },
+  ];
+  const options = {
+    chart: {
+      type: "line",
+      zoom: {
+        enabled: false,
+      },
+    },
+    dataLabels: {
+      enabled: true,
+    },
+    stroke: {
+      curve: "smooth",
+      width: 3,
+    },
+    colors: ["#008FFB"],
+    //   colors: [COLOR_2],
+    xaxis: {
+      categories: key,
+    },
+  };
+  if (loading) {
+    return (
+      <>
+        <p>Data Loading</p>
+      </>
+    );
+  } else if (error) {
+    return (
+      <>
+        <p>Error:{error}</p>
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <Chart options={options} series={series} height={300} />
+      </div>
+    );
+  }
+};
+
+export default Home;
