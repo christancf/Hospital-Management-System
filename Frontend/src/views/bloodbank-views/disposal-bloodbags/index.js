6e68a00ee7b36dbc9ff9bdcfc60979aedf384690
Shavidini Ekanayake <shavidilunika10s@gmail.com>
2022-05-19 22:18:29 +0530

    api for blood bag exire details

--- Frontend/src/views/bloodbank-views/disposal-bloodbags/index.js
+++ Frontend/src/views/bloodbank-views/disposal-bloodbags/index.js
@@ -1,13 +1,82 @@
-import React from 'react'
+import React, { useState, useEffect,Modal,Component } from 'react';
+import { Table, Typography, Spin, Button, Divider, Tag } from 'antd';
 import Chart from "react-apexcharts";
 import { COLORS} from 'constants/ChartConstant';
 
-const Home = () => {
-	return (
+import bloodBankService from 'services/BloodBankService'
+
+const { Title } = Typography
+
+const DisposaBloodBag = () => {
+
+	const state = { visible: false };
+
+	function toTimestamp(strDate) {
+		var datum = Date.parse(strDate);
+		return datum / 1000;
+	}
+
+	const showModal = () => {
+		this.setState({
+		  visible: true,
+		});
+	  };
+
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+	const [data, setData] = useState();
+
+	useEffect(() => {
+		bloodBankService.readBloodDetails().then((resp) => {
+			setData(resp.payload);
+			setLoading(false);
+
+		}).catch((err) => {
+			setLoading(false);
+			setError(true);
+			setData();
+		});
+	}, []);
+
+
+	
+	if (loading) {
+		return (
+			<>
+				<center>
+					<Spin size="large" tip="Loading..." delay={500} spinning={loading} />
+				</center>
+
+			</>
+		)
+	}
+	else if (error) {
+
+		return (
+			<>
+				<center>
+					<Spin size="large" tip="Loading..." delay={500} spinning={loading} />
+				</center>
+
+			</>
+		)
+
+	}
+	else{
+
+
+
+		
+		return (
 		<div>
-			A
+			<Title>Details Of Disposal Blood Bags</Title>
 		</div>
 	)
+	}
+
+
+
+	
 }
 
-export default Home
+export default DisposaBloodBag
