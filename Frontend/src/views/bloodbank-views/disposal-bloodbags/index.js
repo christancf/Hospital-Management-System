b392ff6e399f8177fde1aec1421151c7e519a00d
Shavidini Ekanayake <shavidilunika10s@gmail.com>
2022-05-20 15:03:50 +0530

    Completed disposal blood bag frontend

--- Frontend/src/views/bloodbank-views/disposal-bloodbags/index.js
+++ Frontend/src/views/bloodbank-views/disposal-bloodbags/index.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect,Modal,Component } from 'react';
+import React, { useState, useEffect, Modal, Component } from 'react';
 import { Table, Typography, Spin, Button, Divider, Tag } from 'antd';
 import Chart from "react-apexcharts";
-import { COLORS} from 'constants/ChartConstant';
+import { COLORS } from 'constants/ChartConstant';
 
 import bloodBankService from 'services/BloodBankService'
 
@@ -18,16 +18,16 @@ const DisposaBloodBag = () => {
 
 	const showModal = () => {
 		this.setState({
-		  visible: true,
+			visible: true,
 		});
-	  };
+	};
 
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
 	const [data, setData] = useState();
 
 	useEffect(() => {
-		bloodBankService.readBloodDetails().then((resp) => {
+		bloodBankService.expireBagDetails().then((resp) => {
 			setData(resp.payload);
 			setLoading(false);
 
@@ -39,7 +39,7 @@ const DisposaBloodBag = () => {
 	}, []);
 
 
-	
+
 	if (loading) {
 		return (
 			<>
@@ -62,21 +62,147 @@ const DisposaBloodBag = () => {
 		)
 
 	}
-	else{
+	else {
+		console.log(data);
+		for (var i = 0; i < data.length; i++) {
+			data[i].donateDate = new Date(data[i].donateDate).toLocaleDateString()
+			data[i].expireDate = new Date(data[i].expireDate).toLocaleDateString()
+		}
+
+		const bloodBags = data.map((response) => {
+
+			console.log(response)
+			return {
+				bagId: response.bagId,
+				donorName: response.donorName,
+				donorNIC: response.donorNIC,
+				donationNumber: response.donationNumber,
+				expireDate: response.expireDate,
+				donateDate: response.donateDate,
+				place: response.place,
+				tags: response.bloodGroup
+			}
+		})
+
+		const columns = [
+			{
+				title: 'Bag ID',
+				dataIndex: 'bagId',
+			},
+			{
+				title: 'Donors Name',
+				dataIndex: 'donorName',
+				// render: text => <a>{text}</a>,
+			},
+			// {
+			// 	title: 'Donation Number',
+			// 	dataIndex: 'donationNumber',
+			// },
+			{
+				title: 'Donate Date',
+				dataIndex: 'donateDate'
+			},
+			{
+				title: 'Expire Date',
+				dataIndex: 'expireDate',
+				// render: text => <a>{text}</a>,
+				color: 'red'
+			},
+			// {
+			// 	title: 'Place',
+			// 	dataIndex: 'place'
+			// },
+			{
+				title: 'Blood Group',
+				dataIndex: 'tags',
+				key: 'tags',
+
+				render: tags => (
+					<span>
+						{tags == "A+" &&
+							<Tag color='purple' key={tags}>
+								{tags.toUpperCase()}
+							</Tag>
+						}
+						{tags == "A-" &&
+							<Tag color='pink' key={tags}>
+								{tags.toUpperCase()}
+							</Tag>
+						}
+						{tags == "B+" &&
+							<Tag color='gold' key={tags}>
+								{tags.toUpperCase()}
+							</Tag>
+						}
+						{tags == "B-" &&
+							<Tag color='orange' key={tags}>
+								{tags.toUpperCase()}
+							</Tag>
+						}
+						{tags == "O+" &&
+							<Tag color='blue' key={tags}>
+								{tags.toUpperCase()}
+							</Tag>
+
+						}
+						{tags == "O-" &&
+							<Tag color='geekblue' key={tags}>
+								{tags.toUpperCase()}
+							</Tag>
+
+						}
+						{tags == "AB+" &&
+							<Tag color='green' key={tags}>
+								{tags.toUpperCase()}
+							</Tag>
+
+						}
+						{tags == "AB-" &&
+							<Tag color='lime' key={tags}>
+								{tags.toUpperCase()}
+							</Tag>
+
+						}
+					</span>
+				),
+			},
+			{
+				title: 'Action',
+				key: 'action',
+				render: (text, record) => (
+
+					// <div class="ant-dropdown-trigger ellipsis-dropdown ant-dropdown-open">
+					// 	<span role="img" aria-label="ellipsis" class="anticon anticon-ellipsis">
+					// 		<svg viewBox="64 64 896 896" focusable="false" data-icon="ellipsis" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+					// 			<path d="M176 511a56 56 0 10112 0 56 56 0 10-112 0zm280 0a56 56 0 10112 0 56 56 0 10-112 0zm280 0a56 56 0 10112 0 56 56 0 10-112 0z"></path>
+					// 		</svg>
+					// 	</span>
+					// </div>
+					<span>
+						<a >Delete </a>
+						{/* <a href={`../bloodbank/update-details?bagId=${record.bagId}`}>Edit</a> */}
+						{/* <a onClick={() => { showModal(record.bagId) }}>View More</a> */}
+						{/* <a onClick={showModal}>View More</a> */}
+						<Divider type="vertical" />
+
+					</span>
+				),
+			},
+		];
 
 
-
-		
 		return (
-		<div>
-			<Title>Details Of Disposal Blood Bags</Title>
-		</div>
-	)
+			<div>
+				<Title>Details Of Disposal Blood Bags</Title>
+				<br></br>
+				<Table columns={columns} dataSource={bloodBags} />
+			</div>
+		)
 	}
 
 
 
-	
+
 }
 
 export default DisposaBloodBag
