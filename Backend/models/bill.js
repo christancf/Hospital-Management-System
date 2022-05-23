827bb1348b82771429c90abc3642ea2f163460b5
osanda <it20633554@my.sliit.lk>
2022-05-23 19:05:29 +0530

    add transation backend change

--- Backend/models/bill.js
+++ Backend/models/bill.js
@@ -1,39 +1,33 @@
 const mongoose = require('mongoose');
 
+
+
 const dataSchema = new mongoose.Schema({
 
-    patient_name: {
-        required: true,
-        type: String
-    },
-    patient_id: {
-        required: true,
-        type: String
-    },
-    room_charges: {
-        required: true,
-        type: Number
-    },
-    item_charges: {
-        required: true,
-        type: Number
-    },
-    doctor_charges: {
-        required: true,
-        type: Number
-    },
-    tax: {
-        required: true,
-        type: Number
-    },
-    total: {
-        required: true,
-        type: Number
-    },
-    status: {
-        required: true,
-        type: String
-    }
+
+
+patient_name: {
+required: true,
+type: String
+},
+patient_id: {
+required: true,
+type: String
+},
+room_charges: {
+required: true,
+type: Number
+},
+item_charges: {
+required: true,
+type: Number
+},
+status: {
+required: true,
+type: String
+}
 })
+
+
 
 module.exports = mongoose.model('bills', dataSchema)
--- Backend/routes/billing.js
+++ Backend/routes/billing.js
@@ -3,98 +3,262 @@ const transactionModel = require('../models/transaction');
 const patientModel = require('../models/patient');
 const itemModel = require('../models/item');
 const billModel = require('../models/bill');
+const roomModel = require('../models/ward');
 var router = express.Router();
 const auth = require("../middleware/auth");
 
 
-const insertTransaction = async (req, res, next, billId) => {
+const insertTransaction = async (req, res,next,billId) => {
 
-  const bill = new transactionModel({
-    patientId: req.body.patientId,
-    type: req.body.type,
-    itemId: req.body.itemId,
-    qty: req.body.qty,
-    billId: billId,
-    patientName: req.body.patientName,
-    roomCharges: req.body.roomCharges,
-    itemCharges: req.body.itemCharges,
-    doctorCharges: req.body.doctorCharges,
-    tax: req.body.tax,
-    total: req.body.total
-  })
+  total = req.body.count * req.body.charge
+  type  = req.body.type
+  transactionModel.findOne({
+    billId:billId,
+    type:req.body.type,
+    id:req.body.id
+  }).then((resp) => {
+    if (resp == null){
 
-  try {
-
-    bill.save().then(async () => {
-
-      const decrementCount = -1 * req.body.qty;
-      const response = await itemModel.findOneAndUpdate({
-        id: req.body.itemId
-      },
-        {
-          $inc: {
-            total_quantity: decrementCount
+      const bill = new transactionModel({
+        patientId: req.body.patientId,
+        type: req.body.type,
+        id: req.body.id,
+        count: req.body.count,
+        billId: billId,
+        patientName: req.body.patientName,
+        charge: req.body.charge,
+        total: total
+      })
+    
+      try {
+    
+        bill.save().then(async () => {
+    
+          if(req.body.type=="room"){
+            billModel.findOneAndUpdate(
+              {
+                _id: billId
+              },
+              {
+                $inc: {
+                  room_charges: total,
+                }
+              }
+              ).then(() => {
+    
+                res.status(200).json(
+                  {
+                    succuss: true,
+                    message: 'Transaction Insertion and bill update succussfull',
+                    payload: {}
+                  }
+                );
+              }).catch((Error) => {
+    
+                res.status(400).json(
+                  {
+                    succuss: false,
+                    message: "62"+Error.message,
+                    payload: {}
+                  }
+                );
+    
+              })
           }
-        }).then((response) => {
+    
+          if(type == "item")
+          {
+            const decrementCount = -1 * req.body.count;
+            itemModel.findOneAndUpdate({
+              id: req.body.id
+            },
+              {
+                $inc: {
+                  total_quantity: decrementCount
+                }
+            })
+            .then((response) => {
+              billModel.findOneAndUpdate(
+                {
+                  _id: billId
+                },
+                {
+                  $inc: {
+                    item_charges: total,
+                  }
+                }
+              ).then(() => {
+    
+                res.status(200).json(
+                  {
+                    succuss: true,
+                    message: 'Transaction Insertion and inventory update succussfull',
+                    payload: {}
+                  }
+                );
+              }).catch((billError) => {
+    
+                res.status(400).json(
+                  {
+                    succuss: false,
+                    message: "105"+billError.message,
+                    payload: {}
+                  }
+                );
+    
+              })
+    
+    
+    
+            }).catch((err) => {
+              res.status(400).json(
+                {
+                  succuss: false,
+                  message: err.message,
+                  payload: {}
+                }
+              );
+    
+            });}
+          
+          
+      
+    
+    
+        }).catch((error) => {
+          res.status(400).json(
+            {
+              succuss: false,
+              message: "133"+error.message,
+              payload: {}
+            }
+          );
+        });
+      } catch (error) {
+        res.status(400).json(
+          {
+            succuss: false,
+            message: "142"+error.message,
+            payload: {}
+          }
+        );
+      }
 
+    }
+    else{
+      // implement what to do if the item is already in transaction
+      transactionModel.findOneAndUpdate({
+        _id:resp._id
+      },{
+        $inc:{
+          count:req.body.count,
+          total:total
+        }
+      }).then(async () => {
+    
+        if(req.body.type=="room"){
           billModel.findOneAndUpdate(
             {
               _id: billId
             },
             {
               $inc: {
-                room_charges: req.body.roomCharges,
-                item_charges: req.body.itemCharges,
-                doctor_charges: req.body.doctorCharges,
-                total: req.body.total,
-                tax: req.body.tax
+                room_charges: total,
               }
             }
-          ).then(() => {
-
-            res.status(200).json(
-              {
-                succuss: true,
-                message: 'Transaction Insertion and inventory update succussfull',
-                payload: {}
+            ).then(() => {
+  
+              res.status(200).json(
+                {
+                  succuss: true,
+                  message: 'Transaction Insertion and bill update succussfull',
+                  payload: {}
+                }
+              );
+            }).catch((Error) => {
+  
+              res.status(400).json(
+                {
+                  succuss: false,
+                  message: "184"+Error.message,
+                  payload: {}
+                }
+              );
+  
+            })
+        }
+  
+        if(type == "item")
+        {
+          const decrementCount = -1 * req.body.count;
+          itemModel.findOneAndUpdate({
+            id: req.body.id
+          },
+            {
+              $inc: {
+                total_quantity: decrementCount
               }
-            );
-          }).catch((billError) => {
-
+          })
+          .then((response) => {
+            billModel.findOneAndUpdate(
+              {
+                _id: billId
+              },
+              {
+                $inc: {
+                  item_charges: total,
+                }
+              }
+            ).then(() => {
+  
+              res.status(200).json(
+                {
+                  succuss: true,
+                  message: 'Transaction Insertion and inventory update succussfull',
+                  payload: {}
+                }
+              );
+            }).catch((billError) => {
+  
+              res.status(400).json(
+                {
+                  succuss: false,
+                  message: billError.message,
+                  payload: {}
+                }
+              );
+  
+            })
+  
+  
+  
+          }).catch((err) => {
             res.status(400).json(
               {
                 succuss: false,
-                message: billError.message,
+                message: err.message,
                 payload: {}
               }
             );
-
-          })
-
-
-
-        }).catch((err) => {
-          res.status(400).json(
-            {
-              succuss: false,
-              message: err.message,
-              payload: {}
-            }
-          );
-
-        });
-
-
-    }).catch((error) => {
-      res.status(400).json(
-        {
-          succuss: false,
-          message: error.message,
-          payload: {}
-        }
-      );
-    });
-  } catch (error) {
+  
+          });}
+        
+        
+    
+  
+  
+      }).catch((error) => {
+        res.status(400).json(
+          {
+            succuss: false,
+            message: error.message,
+            payload: {}
+          }
+        );
+      });
+    
+    }
+  }).catch((error) => {
     res.status(400).json(
       {
         succuss: false,
@@ -102,9 +266,11 @@ const insertTransaction = async (req, res, next, billId) => {
         payload: {}
       }
     );
-  }
 
+  });
 }
+
+
 //add bill details
 router.post('/add-details', async (req, res, next) => {
 
@@ -121,9 +287,6 @@ router.post('/add-details', async (req, res, next) => {
           patient_id: req.body.patientId,
           room_charges: 0,
           item_charges: 0,
-          doctor_charges: 0,
-          tax: 0,
-          total: 0,
           status: 'pending'
         }
       )
@@ -153,9 +316,6 @@ router.post('/add-details', async (req, res, next) => {
 });
 
 router.get('/patient', async (req, res, next) => {
-
-
-
   try {
     const response = await patientModel.find({status:"true"}).then((response) => {
 
@@ -252,11 +412,44 @@ router.get('/transactions', async (req, res, next) => {
 
 
 router.get('/items', async (req, res, next) => {
-
-
-
   try {
-    const response = await itemModel.find({}).then((response) => {
+    const response = await itemModel.find({total_quantity:{$ne:0}}).then((response) => {
+
+      res.status(200).json(
+        {
+          succuss: true,
+          message: 'Retirval succussfull',
+          payload: response
+        }
+      );
+
+    }).catch((error) => {
+      res.status(400).json(
+        {
+          succuss: false,
+          message: error.message,
+          payload: {}
+        }
+      );
+
+    });
+
+
+  }
+  catch (error) {
+    res.status(400).json(
+      {
+        succuss: false,
+        message: error.message,
+        payload: {}
+      }
+    );
+  }
+
+});
+router.get('/rooms', async (req, res, next) => {
+  try {
+    const response = await roomModel.find({}).then((response) => {
 
       res.status(200).json(
         {
@@ -292,9 +485,6 @@ router.get('/items', async (req, res, next) => {
 });
 
 router.get('/bills', async (req, res, next) => {
-
-
-
   try {
     const response = await billModel.find({
       patient_id: req.query.patient
@@ -334,8 +524,6 @@ router.get('/bills', async (req, res, next) => {
 });
 
 router.get('/all-bills', async (req, res, next) => {
-
-
 
   try {
     const response = await billModel.find({
