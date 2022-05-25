893539172c5f8c9ab1b177d0c13e8d00008942b6
Susith Rupasinghe <susith03program@gmail.com>
2022-05-25 12:58:31 +0530

    JWT escaped for developments

--- Frontend/src/auth/FetchInterceptor.js
+++ Frontend/src/auth/FetchInterceptor.js
@@ -30,27 +30,27 @@ const ENTRY_ROUTE = '/'
 const TOKEN_PAYLOAD_KEY = 'x-access-token'
 const PUBLIC_REQUEST_KEY = 'public-request'
 
-// API Request interceptor
-service.interceptors.request.use(config => {
-	const jwtToken = localStorage.getItem(AUTH_TOKEN)
+// // API Request interceptor
+// service.interceptors.request.use(config => {
+// 	const jwtToken = localStorage.getItem(AUTH_TOKEN)
 	
-  if (jwtToken) {
-    config.headers[TOKEN_PAYLOAD_KEY] = jwtToken
-  }
+//   if (jwtToken) {
+//     config.headers[TOKEN_PAYLOAD_KEY] = jwtToken
+//   }
 
-  if (!jwtToken && !config.headers[PUBLIC_REQUEST_KEY]) {
-		history.push(ENTRY_ROUTE)
-		window.location.reload();
-  }
+//   if (!jwtToken && !config.headers[PUBLIC_REQUEST_KEY]) {
+// 		history.push(ENTRY_ROUTE)
+// 		window.location.reload();
+//   }
 
-  return config
-}, error => {
-	// Do something with request error here
-	notification.error({
-		message: 'Error'
-	})
-  Promise.reject(error)
-})
+//   return config
+// }, error => {
+// 	// Do something with request error here
+// 	notification.error({
+// 		message: 'Error'
+// 	})
+//   Promise.reject(error)
+// })
 
 // API respone interceptor
 service.interceptors.response.use( (response) => {
